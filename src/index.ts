96dc091081b0b50d1d2cf955bc9112c7280701ce
Gan Shuai <sgan@thoughworks.com>
2019-10-21 16:55:06 +0800

    it's work on IE

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,113 @@
 import * as webpack from 'webpack'
 import * as express from 'express'
 import * as http from 'http'
-import { build, featureMap } from 'js-build-by-feature-map'
+import * as path from 'path'
+import * as featureTestHtml from 'js-feature-test/dist/featureTestHtml'
+import * as bodyParser from 'body-parser'
+import { build, featureMap, builderConfig } from 'js-build-by-feature-map'
 
-interface config {
+export interface config {
   route: string
   webpackConfig: webpack.Configuration
 }
 
-interface serviceConfig {
+export interface singleConfig {
+  route: string
+  webpackConfig: webpack.Configuration
+  buildConfig: builderConfig
+  autoTest?: boolean
+  featureMap?: featureMap
+}
+
+export interface serviceConfig {
   configs: config[]
   featureMap: featureMap
 }
 
-interface dynamicProperty {
+export interface dynamicProperty {
   [key: string]: any
 }
 
-export function createBuild(webpackConfig: webpack.Configuration, featureMap: featureMap = {}) {
-  return async function toBuild(req: express.Request) {
-    const cookieFeatureMap = getFeatureMapFromHeaders(req.headers)
+export function createBuild(
+  webpackConfig: webpack.Configuration,
+  buildConfig: builderConfig,
+  featureMap: featureMap = {},)
+{
+  return async (cookieFeatureMap: featureMap) => {
     const allFeatureMap = mergeFeatureMap(cookieFeatureMap, featureMap)
-  
-    return await build(allFeatureMap, webpackConfig)
+    return await build(allFeatureMap, webpackConfig, buildConfig)
   }
 }
 
-export function service(app: express.Express, serviceConfig: serviceConfig) {
-  serviceConfig.configs.forEach(config => {
-    const buildFn = createBuild(config.webpackConfig, serviceConfig.featureMap)
-    app.use(config.route, (req: express.Request) => {
-      buildFn(req)
-    })
+// TODO: 目前webpack必须处于watch模式，否则每一个js的访问都会触发重新打包
+export function service(app: express.Express, serviceConfig: singleConfig) {
+  // serviceConfig serviceConfig 多个单文件模式
+  // serviceConfig.configs.forEach(config => {
+  //   const buildFn = createBuild(config.webpackConfig, serviceConfig.featureMap)
+  //   app.use(config.route, (req: express.Request) => {
+  //     buildFn(req)
+  //   })
+  // })
+
+  serviceConfig = getServiceConfig(serviceConfig)
+  const _build = createBuild(serviceConfig.webpackConfig, serviceConfig.buildConfig, serviceConfig.featureMap || {})
+  const featureTestUrl = '/feature-test'
+  app.use(bodyParser.json())
+  app.use(serviceConfig.route, async (req, res, next) => {
+    // if(/\/workflow\.js$/ig.test(req.originalUrl)) {
+      // await build(req)
+    // }
+    const cookieFeatureMap = getFeatureMapFromHeaders(req.headers)
+    const buildStatus = await _build(cookieFeatureMap || {})
+    const distPath = buildStatus.outputPath || path.resolve(__dirname, '../dist')
+    const distHandle = express.static(distPath)
+    return distHandle(req, res, next)
   })
+
+  app.get('*.html', (req, res, next) => {
+    const cookieFeatureMap = getFeatureMapFromHeaders(req.headers)
+    //如果cookie不存在，则自动跳转到feature-test页面
+    if(!cookieFeatureMap && serviceConfig.autoTest && isHtml(req.originalUrl)) {
+      return res.redirect(featureTestUrl + '?back=' + req.originalUrl)
+    }
+
+    return next()
+  })
+  
+  app.get(featureTestUrl, (req, res) => {
+    res.setHeader('Content-Type', 'text/html')
+    res.send(featureTestHtml)
+  })
+
+  app.post(featureTestUrl, (req, res) => {
+    const featureMapString = JSON.stringify(req.body)
+    res.cookie('jsFeatureTest', featureMapString, {
+      //过期时间为15天
+      expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 15),
+      path: '/',
+      signed: false
+    })
+    res.send()
+  })
+}
+
+function isHtml(requestUrl: string): boolean {
+  return /\.html\b/.test(requestUrl)
+}
+
+function getServiceConfig(serviceConfig: singleConfig) {
+  const defaultConfig: singleConfig = {
+    route: '',
+    webpackConfig: null,
+    buildConfig: null,
+    autoTest: true,
+    featureMap: {}
+  }
+
+  return {
+    ...defaultConfig,
+    ...serviceConfig
+  }
 }
 
 function mergeFeatureMap(...featureMaps: featureMap[]): featureMap {
@@ -51,8 +126,15 @@ function mergeFeatureMap(...featureMaps: featureMap[]): featureMap {
 
 
 function getFeatureMapFromHeaders(headers: http.IncomingHttpHeaders) {
-  const cookie = parseCookie(headers.cookie)
-  return JSON.parse(cookie['jsFeatureTest'] || '{}')
+  try{
+    const cookie = parseCookie(headers.cookie || '')
+
+    if(cookie && cookie['jsFeatureTest']) {
+      return JSON.parse(decodeURIComponent(cookie['jsFeatureTest']))
+    }
+  }catch(e) {console.error(e)}
+  
+  return null
 }
 
 function parseCookie(cookies: string) {
